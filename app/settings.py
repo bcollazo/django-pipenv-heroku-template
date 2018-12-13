9c3039db020d77ff073721beb1d470dc14dba8f7
Bryan Collazo <bcollazo2010@gmail.com>
2018-12-13 12:35:57 -0400

    Connect to Heroku

--- app/settings.py
+++ app/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Make Heroku-ready
+import django_heroku
+django_heroku.settings(locals())
